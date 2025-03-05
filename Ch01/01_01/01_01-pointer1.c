b7294ab1c64153ae4e69f75678fc88a0a38f0538
mjahangirmakroo <er.jahaan@gmail.com>
2025-03-05 14:18:07 +0530

    \added

---
+++ Ch01/01_01/01_01-pointer1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	char *ptr;
+
+	/* initialize variables! */
+	a = 'A';
+
+	printf("Value of variable 'a': %c\n",a);
+	/* assign ptr to a's address */
+	ptr = &a;
+	printf("Address of variable 'a': %p\n",ptr);
+	printf("Value of pointer 'ptr': %c\n",*ptr);
+
+	return(0);
+}
+
+/*#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+int main(void) {
+   const size_t x = 150;
+   int a[x];
+   for (size_t i = 0;i < x; ++i)
+   a[i] = i;
+   printf("SIZE_MAX = %lu
+", SIZE_MAX);
+   size_t size = sizeof(a);
+   printf("size = %zu
+", size);
+}
---
+++ Ch04/04_05/04_05-output2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void output(int a[])
+{
+
+	int x;
+printf("This block is executed");
+	for(x=0; x<5; x++)
+		printf("%d\n",a[x]);
+}
+
+int main()
+{
+	int numbers[] = { 3, 5, 7, 9, 11 };
+	void (*fp)(int a[]);
+
+	fp = output;
+	fp(numbers);
+
+	return(0);
+}
